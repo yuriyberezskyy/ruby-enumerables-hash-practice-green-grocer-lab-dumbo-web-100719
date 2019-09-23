2e053874e1ffba2d500406522e43f7c056bc499e
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-23 04:38:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -30,7 +30,7 @@ def apply_coupons(cart, coupons)
         hash.merge!(temp)
       else
         hash["#{item} W/COUPON"][:count] += 1
-        #hash["#{item} W/COUPON"][:price] += coupon_hash[:cost]
+        hash["#{item} W/COUPON"][:price] += coupon_hash[:cost]
       end
       
       hash[item][:count] -= coupon_hash[:num]
