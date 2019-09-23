d42062a58c7f12b6804041882ccdaab260d54471
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-23 04:04:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,7 +18,7 @@ def apply_coupons(cart, coupons)
   result = {}
   cart.each do |food, info|
     coupons.each do |coupon|
-      if food == coupon[:item] && info[:count] >= coupon[:num]
+      if food == coupon[:item] && info[:count] >= coupon
         info[:count] =  info[:count] - coupon[:num]
         if result["#{food} W/COUPON"]
           result["#{food} W/COUPON"][:count] += 1
